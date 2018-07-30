a9b1dc38b15124518d8531b3b8920de35b20b25d
ilias-sp <spiliotisilias@gmail.com>
2018-07-31 00:39:34 +0300

    added step to convert EOL to CRLF

--- SRT_add_remove_offset.php
+++ SRT_add_remove_offset.php
@@ -66,6 +66,8 @@ function do_conversion_main($file_name, $offset)
         $new_line = identify_process_line($line, $offset);
         $processed_srt_contents .= $new_line . PHP_EOL;
     }
+    // change EOL to windows, by https://stackoverflow.com/questions/7836632/how-to-replace-different-newline-styles-in-php-the-smartest-way:
+    $processed_srt_contents = preg_replace('~\R~u', "\r\n", $processed_srt_contents);
     //
     if ( @file_put_contents($file_name, $processed_srt_contents) === FALSE)
     {
